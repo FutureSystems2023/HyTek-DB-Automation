0ea2fa6adc4f54480b46a1d834bab12a003c6909
King Yeung Lee <kingyeung129@hotmail.com>
2023-11-01 10:55:48 +0800

    Release V1.0.0

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import configparser
 import logging
 import os
 
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 log_file_path = os.path.join(os.getcwd(), config["DEFAULT"]["LOG_FILE"])
 
+# Create log file or reinitialize log file if exceed a size
 if not os.path.exists(log_file_path):
     with open(log_file_path, 'w') as fp: 
         pass
@@ -20,3 +22,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)s %(message)s'
 )
+
+# Create uploads folder if not exists
+if not os.path.exists(os.path.join(os.getcwd(), config["DEFAULT"]["UPLOAD_DIR"])):
+    os.mkdir(config["DEFAULT"]["UPLOAD_DIR"])
---
+++ decrypt.py
@@ -0,0 +1,19 @@
+import os
+
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    key = os.getenv("DECRYPTION_KEY")
+    cipher_suite = Fernet(key)  # Initialize the Fernet cipher with the loaded key
+    with open('encrypted_credentials.bin', 'rb') as file:  # Load the encrypted data from the file
+        encrypted_data = file.read()
+    decrypted_data = cipher_suite.decrypt(encrypted_data)  # Decrypt the data and parse as json string
+    data = decrypted_data.decode('utf-8')
+    return data
+
+
+if __file__ == "__main__":
+    main()
